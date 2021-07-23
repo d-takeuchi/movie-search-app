47d31f722a83ff382ed92d0385b659c3752da4e0
d-takeuchi <kuronk.kuro3@gmail.com>
2021-07-23 19:47:00 +0900

    作業中

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,36 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import Header from './components/atoms/Header';
+
+type Movie = {
+  Poster: string;
+  Title: string;
+  Type: string;
+  Year: string;
+};
 
 export default function App() {
-  return <BrowserRouter />;
+  const [movies, setMovies] = useState<Array<Movie>>([]);
+
+  const url = 'http://www.omdbapi.com/?s=man&apikey=d862db0';
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((jsonResponse) => {
+        setMovies(jsonResponse.Search);
+      });
+  }, []);
+
+  return (
+    <>
+      <Header />
+      {movies.map((movie) => (
+        <>
+          <h3>{movie.Title}</h3>
+          <p>{`${movie.Year}年公開`}</p>
+          <img src={movie.Poster} alt="ポスター" />
+        </>
+      ))}
+    </>
+  );
 }
---
+++ src/components/atoms/Header.tsx
@@ -0,0 +1,11 @@
+import React, { memo, VFC } from 'react';
+
+const Header: VFC = memo(() => {
+  return (
+    <header>
+      <h1>映画検索アプリ</h1>
+    </header>
+  );
+});
+
+export default Header;
